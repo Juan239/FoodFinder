6b930198837fd32d955a656e1b1dfe60eacd8975
Juan <juan93620672@gmail.com>
2023-09-27 16:19:05 -0300

    Prueba commit

--- app/src/main/java/com/example/foodfinder_02/MapsActivity.java
+++ app/src/main/java/com/example/foodfinder_02/MapsActivity.java
@@ -34,7 +34,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        //Prueba commit
         // Add a marker in Ovalle and move the camera
         LatLng ovalle = new LatLng(-30.60271665994994, -71.19756580634903);
         mMap.addMarker(new MarkerOptions().position(ovalle).title("Ovalle"));
